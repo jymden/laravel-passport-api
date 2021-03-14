f1b5811acf45829a8608d5655484a37f712383b2
Christoffer Wadensten <cwadensten@gmail.com>
2021-03-14 14:43:00 +0100

    Add custom response to Unauthenticated message

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +38,19 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            $json = [
+                'success' => false,
+                'data' => [],
+                'message' => $exception->getMessage()
+            ];
+            return response()->json($json, 401);
+        }
+
+        return redirect()->guest(route('login'));
     }
 }
